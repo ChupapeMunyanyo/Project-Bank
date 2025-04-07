abecea91c079535d23d2b519e1d64c8d6d0313d7
ChupapeMunyanyo <mikailshah2005@gmail.com>
2025-04-07 20:25:04 +0300

    Update Header.tsx
    improved Header.tsx

--- simplebank/src/components/Header.tsx
+++ simplebank/src/components/Header.tsx
@@ -4,9 +4,6 @@ const Header: React.FC = () => {
   return (
     <header className="header">
       <h1>SimpleBank Subgraph Demo</h1>
-      <p>
-        Тестовое задание для позиции Senior Software Engineer – On-Chain Development в D3 Development
-      </p>
     </header>
   );
 };
